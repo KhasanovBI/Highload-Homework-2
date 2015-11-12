1336e8f6747e353d3bfcbd75bc76a7a3b976fe42
Bulat Khasanov <afti@yandex.ru>
2015-11-13 00:26:15 +0300

    some small changes

--- main.cpp
+++ main.cpp
@@ -3,93 +3,97 @@
 #include <algorithm>
 #include <fstream>
 #include <vector>
+#include <cmath>
+
+#define ONE(x) x x
+#define FIVE(x) ONE(x) ONE(x) ONE(x) ONE(x) ONE(x)
+#define TEN(x) FIVE(x) FIVE(x)
+#define FIFTY(x) TEN(x) TEN(x) TEN(x) TEN(x) TEN(x)
+#define HUNDRED(x) FIFTY(x) FIFTY(x)
+#define FIVE_HUNDRED(x) HUNDRED(x) HUNDRED(x) HUNDRED(x) HUNDRED(x) HUNDRED(x)
+#define THOUSAND(x) FIVE_HUNDRED(x) FIVE_HUNDRED(x)
+#define CACHE_CLEAR_LIST_SIZE (8 * 1024 * 1024)
+#define CYCLES 1000
 
 using namespace std;
-#define CACHE_CLEAR_LIST_SIZE (8 * 1024 * 1024)
-#define RANDOM_ACCESS_ITERATIONS_COUNT 10
+
+struct Pointer {
+    struct Pointer *next;
+};
+
+typedef Pointer testType;
+
+struct TestParams {
+    const char *testName;
+    const char *resultsFileName;
+    size_t initialListSize;
+    size_t maxListSize;
+
+    void (*prepareList)(testType *testList, const size_t testListSize);
+};
 
 void clearCache() {
-    size_t trashListSize = CACHE_CLEAR_LIST_SIZE / sizeof(void *);
-    volatile void **trashList = new volatile void *[trashListSize];
-    volatile void **currentPosition = trashList;
+    size_t trashListSize = CACHE_CLEAR_LIST_SIZE / sizeof(testType *);
+    testType *trashList = new testType[trashListSize];
+    for (size_t i = 0; i < trashListSize; ++i) {
+        trashList[i].next = trashList + ((i + 1) % trashListSize);
+    }
+    testType *currentPosition = trashList;
     for (int i = 1; i < trashListSize; i *= 2) {
-        currentPosition = (volatile void **) trashList[i];
+        currentPosition = currentPosition->next;
     }
     delete[] trashList;
 }
 
-void prepareDirectList(volatile void **testList, const size_t testListSize, const size_t step) {
-    for (size_t i = 0; i < testListSize; i += step) {
-        testList[i] = (void *) (testList + i + step);
+void prepareSequentialList(testType *testList, const size_t testListSize) {
+    for (size_t i = 0; i < testListSize; ++i) {
+        testList[i].next = testList + ((i + 1) % testListSize);
     }
     clearCache();
 }
 
-void directAccessMeasurement(size_t initialListSize, size_t maxListSize) {
-    cout << "Direct access" << endl;
-    ofstream resultsFile;
-    resultsFile.open("directAccessCacheMeasuring.csv");
-    volatile void **testList = new volatile void *[maxListSize];
-    for (size_t step = initialListSize; step < maxListSize; step += 10240) {
-        double measuredTime = 0;
-        size_t DIRECT_ACCESS_ITERATIONS_COUNT = step / initialListSize * 100;
-        for (size_t j = 0; j < DIRECT_ACCESS_ITERATIONS_COUNT; ++j) {
-            prepareDirectList(testList, maxListSize, step);
-            clock_t start = clock();
-            volatile void **currentPosition = testList;
-            for (size_t i = 0; i < maxListSize; i += step) {
-                currentPosition = (volatile void **) *currentPosition;
-            }
-            clock_t stop = clock();
-            measuredTime += (stop - start);
-        }
-        measuredTime /= DIRECT_ACCESS_ITERATIONS_COUNT * (maxListSize / step + (maxListSize % step > 0 ? 1 : 0));
-        cout << "Step size: " << step * sizeof(void *) / 1024 << "Kb, Time: " << measuredTime << endl;
-        resultsFile << step * sizeof(void *) / 1024 << ',' << measuredTime << endl;
-    }
-    delete[] testList;
-    resultsFile.close();
-}
-
-
-void prepareRandomList(volatile char **testList, const size_t testListSize) {
+void prepareRandomList(testType *testList, const size_t testListSize) {
     vector<size_t> randomHelper;
     randomHelper.reserve(testListSize);
     for (size_t i = 0; i < testListSize; ++i) {
         randomHelper.push_back(i);
     }
     random_shuffle(randomHelper.begin(), randomHelper.end());
-    for (size_t i = 0; i < testListSize; ++i) {
-        testList[i] = (char *) (testList + randomHelper[i]);
+    size_t previous = randomHelper[0];
+    size_t current;
+    for (size_t i = 1; i < testListSize; ++i) {
+        current = randomHelper[i];
+        testList[previous].next = testList + current;
+        previous = current;
     }
+    testList[randomHelper.back()].next = testList + randomHelper[0];
     clearCache();
 }
 
-double measureRandomAccess(volatile char **testList, const size_t testListSize) {
+double measure(testType *testList) {
+    testType *currentPosition = testList;
     double averageTime = 0;
-    for (size_t j = 0; j < RANDOM_ACCESS_ITERATIONS_COUNT; ++j) {
-        prepareRandomList(testList, testListSize);
+    for (int count = 0; count < CYCLES; count++) {
         clock_t start = clock();
-        volatile char **currentPosition = testList;
-        for (size_t i = 0; i < testListSize; ++i) {
-            currentPosition = (volatile char **) *currentPosition;
-        }
+        THOUSAND(currentPosition = currentPosition->next;)
         clock_t stop = clock();
-        averageTime += (stop - start);
+        averageTime += stop - start;
     }
-    averageTime /= (RANDOM_ACCESS_ITERATIONS_COUNT * testListSize);
-    return averageTime;
+    return averageTime / 1000 / CYCLES;
 }
 
-void randomAccessMeasurement(size_t initialListSize, size_t maxListSize) {
-    cout << "Random access" << endl;
+void measurement(TestParams &testParams) {
+    cout << testParams.testName << endl;
     ofstream resultsFile;
-    resultsFile.open("randomAccessCacheMeasuring.csv");
-    for (size_t testListSize = initialListSize; testListSize <= maxListSize; testListSize *= 1.15) {
-        volatile char **testList = new volatile char *[testListSize];
-        double measuredTime = measureRandomAccess(testList, testListSize);
-        cout << "List size: " << double(testListSize) * sizeof(void *) / 1024 << "Kb, Time: " << measuredTime << endl;
-        resultsFile << double(testListSize) * sizeof(void *) / 1024 << ',' << measuredTime << endl;
+    resultsFile.open(testParams.resultsFileName);
+    for (size_t testListSize = testParams.initialListSize;
+         testListSize <= testParams.maxListSize; testListSize *= 1.1) {
+        testType *testList = new testType[testListSize];
+        testParams.prepareList(testList, testListSize);
+        double measuredTime = measure(testList);
+        cout << "List size: " << double(testListSize) * sizeof(testType *) / 1024 << "Kb, Time: " << measuredTime <<
+        endl;
+        resultsFile << double(testListSize) * sizeof(testType *) / 1024 << ',' << measuredTime << endl;
         delete[] testList;
     }
     resultsFile.close();
@@ -98,10 +102,25 @@ void randomAccessMeasurement(size_t initialListSize, size_t maxListSize) {
 int main(int argc, char *argv[]) {
     size_t initialListMemorySize = 1024;
     size_t maxListMemorySize = 8 * 1024 * initialListMemorySize;
-    size_t initialListSize = initialListMemorySize / sizeof(void *);// size - number of elements, not memory size
-    size_t maxListSize = maxListMemorySize / sizeof(void *);
-    randomAccessMeasurement(initialListSize, maxListSize);
-    directAccessMeasurement(initialListSize, maxListSize);
+    size_t initialListSize = initialListMemorySize / sizeof(testType *);// size - number of elements, not memory size
+    size_t maxListSize = maxListMemorySize / sizeof(testType *);
+
+    TestParams randomAccessParams;
+    randomAccessParams.testName = "Random access";
+    randomAccessParams.initialListSize = initialListSize;
+    randomAccessParams.maxListSize = maxListSize;
+    randomAccessParams.prepareList = prepareRandomList;
+    randomAccessParams.resultsFileName = "randomAccessCacheMeasuring.csv";
+
+    TestParams sequentialAccessParams;
+    sequentialAccessParams.testName = "Sequential access";
+    sequentialAccessParams.initialListSize = initialListSize;
+    sequentialAccessParams.maxListSize = maxListSize;
+    sequentialAccessParams.prepareList = prepareSequentialList;
+    sequentialAccessParams.resultsFileName = "sequentialAccessCacheMeasuring.csv";
+
+    measurement(randomAccessParams);
+    measurement(sequentialAccessParams);
     system("./plot.py &");
     return 0;
 }
